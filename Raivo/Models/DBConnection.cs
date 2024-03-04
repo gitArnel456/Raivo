a1a3395c18ebcb7cca9aa4b5a4054a5d451eb358
gitArnel456 <arnelmihaja@gmail.com>
2024-03-04 11:45:06 +0300

    jesus

---
+++ Raivo/Models/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Raivo.Models
+{
+    public class DBConnection
+    {
+        public static NpgsqlConnection connectionstring = new NpgsqlConnection
+            (ConfigurationManager.ConnectionStrings["tacheDBConnection"].ConnectionString);
+
+    }
+}[]
