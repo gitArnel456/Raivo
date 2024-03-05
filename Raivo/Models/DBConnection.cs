7e8c237c22be8999a035db298daa150030ce50ef
gitArnel456 <arnelmihaja@gmail.com>
2024-03-05 10:46:32 +0300

    test

--- Raivo/Models/DBConnection.cs
+++ Raivo/Models/DBConnection.cs
@@ -12,5 +12,23 @@ namespace Raivo.Models
         public static NpgsqlConnection connectionstring = new NpgsqlConnection
             (ConfigurationManager.ConnectionStrings["tacheDBConnection"].ConnectionString);
 
+        public static bool verifylogin(Login login)
+        {
+            var req = "SELECT \"nomutilisateur\", \"motdepasse\" FROM public.\"login\" WHERE \"nomutilisateur\"='" + login.Nomutilisateur + "' AND \"motdepasse\"= '" + login.Motdepasse + "';";
+            var hasUser = false;
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                var reader = cmd.ExecuteReader();
+                hasUser = reader.HasRows;
+                connectionstring.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return hasUser;
+        }
     }
-}[]
+}
---
+++ Raivo/Models/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Raivo.Models
+{
+    public class Login
+    { 
+        private int _id;
+        private string _nomutilisateur;
+        private string _motdepasse;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Nomutilisateur { get => _nomutilisateur; set => _nomutilisateur = value; }
+        public string Motdepasse { get => _motdepasse; set => _motdepasse = value; }
+
+        public Login(int id, string nomutilisateur, string motdepasse)
+        {
+            _id = id;
+            _nomutilisateur = nomutilisateur;
+            _motdepasse = motdepasse;
+        }
+        public Login()
+        {
+
+        }
+    }
+}
